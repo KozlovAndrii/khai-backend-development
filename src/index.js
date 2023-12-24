fcf7ee5d0e8b5f56ddd587181c05a97ef8d25d29
Andrii Kozlov <andrii.kozlov@raccoongang.com>
2023-12-24 13:34:37 +0200

    Feature: Lab #6 - Express server #2
    Additional task - find product by ID completed. Middlewares are used

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const productsRouter = require("./products.routes");
+const { logRequest } = require("./middleware");
+const { errorResponder } = require("./error.middleware");
+
 dotenv.config();
 
 const port = process.env.PORT;
+
 const app = express();
-
-const products = [
-  { id: 1, name: "Product 1", brand: "Brand A" },
-  { id: 2, name: "Product 2", brand: "Brand B" },
-  { id: 3, name: "Product 3", brand: "Brand A" },
-];
-
-app.get("/", (req, res) => {
-  res.send("response for GET request");
-});
-
-app.get("/products/:brand", (req, res) => {
-  const { brand } = req.params;
-
-  const filteredProducts = products.filter(
-    (product) => product.brand === brand
-  );
-
-  res.json(filteredProducts);
-});
-
-app.listen(port, () => console.log(`Server started on port: ${port}`));
+app
+  .use(logRequest)
+  .use(productsRouter)
+  .use(errorResponder)
+  .listen(port, () => console.log(`Server started on port: ${port}`));
